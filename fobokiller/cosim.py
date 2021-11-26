b7c4c8cb8d5f4e93051a33c5653870c8f90215d7
Arthur Collard <arthur.collard@hotmail.fr>
2021-11-26 14:48:23 +0100

    cosim updated

--- fobokiller/cosim.py
+++ fobokiller/cosim.py
@@ -5,14 +5,14 @@ import tensorflow
 import pickle
 import os
 
-path_sentences = os.path.join(os.path.dirname(__file__),'data/scrapping_cleaned_sentences.csv')
+path_full_dataset = os.path.join(os.path.dirname(__file__),'data/scrapping_cleaned_sentences.csv')
 path_model = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'model')
 path_embed=os.path.join(os.path.dirname(os.path.dirname(__file__)),'embeddings.pkl')
 
-sentences_df = pd.read_csv(path_sentences,index_col=0)
+df_full = pd.read_csv(path_full_dataset,index_col=0)
 model = SentenceTransformer(path_model)
 
-df_resto = sentences_df.groupby('alias').agg({ 'rate':'mean',
+df_all_resto = df_full.groupby('alias').agg({ 'rate':'mean',
                                                 'review':'nunique'
                                             })
 
@@ -26,29 +26,40 @@ def compute_sim_df(text, embedding, n_prox=None, min_review=0):
     input_encoded = model.encode(text)
     similarities = util.cos_sim(input_encoded, np.array(embedding))
 
-    df_sim = sentences_df.assign(sim=similarities.T)
+    df_sim = df_full.assign(sim=similarities.T)
 
     if n_prox:
-        df_filtered = df_sim.sort_values('sim', ascending=False)[:n_prox]
+        df_sentences = df_sim.sort_values('sim', ascending=False)[:n_prox]
     else:
-        df_filtered = df_sim.sort_values('sim', ascending=False)
+        df_sentences = df_sim.sort_values('sim', ascending=False)
 
-    df_agg = df_filtered.groupby('alias').agg({
+    df_agg = df_sentences.groupby('alias').agg({
         'rate': 'mean',
         'review': 'nunique',
-        'review_sentences':'   ',
+        'review_sentences':'first',
         'review_clean':lambda txt: ' // '.join(txt),
         'sim':'mean'
     })
 
-    df_output = df_agg.merge(df_resto,
+    df_resto = df_agg.merge(df_all_resto,
                              on='alias',
                              how='left',
                              suffixes=('_filtered', '_all')).reset_index()
 
-    df_output['ratio'] = df_output['review_filtered'] / df_output['review_all']
+    df_resto['ratio'] = df_resto['review_filtered'] / df_resto['review_all']
 
-    return df_output[df_output['review_all'] > min_review] , df_filtered
+    df_resto = df_resto.sort_values('ratio')
+    df_resto = df_resto[df_resto['review_all'] > min_review]
+
+    df_final = df_sentences.merge(df_resto,
+                                 on='alias',
+                                 how='left',
+                                 suffixes=('_s', '_r'))
+
+    df_final.drop(columns=['review', 'review_clean_s', 'review_sentences_r'],
+                 inplace=True)
+
+    return df_final
 
 
 if __name__ == '__main__':
