6a23c8a9d166c0bdae34d9c6536efafd359af561
nmanoharan <nmanoharan@me.com>
2021-11-27 12:39:23 +0100

    one details resté

--- fobokiller/api/fast.py
+++ fobokiller/api/fast.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
-resto_list = pd.read_csv("gs://fobokiller-722/final_resto_list.csv")
+resto_list = pd.read_csv("gs://fobokiller-722/final_resto_list.csv",
+                         index_col=0)
 print(resto_list.shape)
 
 app = FastAPI()
@@ -16,28 +17,15 @@ app.add_middleware(
 )
 
 
-
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
-
 @app.get("/")
 def index():
     return {"greeting": "Hello world"}
 
+
 #Input list of restaurant (alias)
 # details
 @app.get("/details")
-def get_details(alias) :
-    print(type(alias))
+def get_details(alias):
+    alias = [alias]
     pd_liste = resto_list.loc[resto_list['alias'].isin(alias), :]
     return pd_liste.to_dict()
